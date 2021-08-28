d3a3f23940461360ad4bfc42a9fa2c1f15901f97
Vishnu V <vishnukpn884@gmail.com>
2021-08-28 23:06:37 +0530

    Updates

--- src/action/notification.js
+++
@@ -1,8 +0,0 @@
-import { NEW_NOTIFICATION } from "./actionType";
-
-export const newnotification = (payload) => {
-  return {
-    type: NEW_NOTIFICATION,
-    payload,
-  };
-};
--- src/utils/url.js
+++
@@ -1,6 +0,0 @@
-export const server = "http://localhost:7000/";
-export const serImg = "http://localhost:7000/img/";
-
-export const createJob = "/job/create";
-export const getJOb = "/job/getJob";
-export const addSkill = "/skill/insert";
